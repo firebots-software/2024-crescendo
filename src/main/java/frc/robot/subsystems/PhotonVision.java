eb4477062ad95c70e5e2331094c5b200a0ee2487
Warzone <yajwin.grover@gmail.com>
2024-01-18 18:57:55 -0800

    Distance is hella accurate

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.annotation.Target;
 import java.sql.Time;
 import java.util.List;
 import java.util.Optional;
@@ -38,140 +39,139 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PhotonVision extends SubsystemBase {
-   private static PhotonVision instance; 
+    Pose3d savedResult = new Pose3d(0.0,0.0,0.0,new Rotation3d(0.0,0.0,0.0));
+    private static PhotonVision pvisioninstance;
+    PhotonCamera camera = new PhotonCamera("FrontCam");
+    Transform3d robotToCam = new Transform3d(new Translation3d(0.5, -0.25, 0.25), new Rotation3d(0, 0, 0));
+    AprilTagFieldLayout aprilTagFieldLayout;
+    PhotonPoseEstimator photonPoseEstimator;
 
-   private PhotonCamera camera = new PhotonCamera("FrontCam");
-   
-   private AprilTagFieldLayout aprilTagFieldLayout;
-   private PhotonPoseEstimator photonPoseEstimator;
-   private PhotonPipelineResult result;
-   private PhotonTrackedTarget target;
+    private static double cameraHeight = 0.3;
+    private static double targetHeight = 1;
+    private static double cameraPitch = Math.PI / 8;
 
-   private static double cameraHeight = 0.3;
-   private static double targetHeight = 1;
-   private static double cameraPitch = Math.PI/8;
-   private Transform3d robotToCam = new Transform3d(new Translation3d(0.5, -0.25, cameraHeight), new Rotation3d(0, cameraPitch, 0));
- 
-   // Need to set up gyro correctly; this is most likely wrong
-   AnalogGyro gyro = new AnalogGyro(0);
+    PhotonPipelineResult pipeline = getPipeline();
+    PhotonTrackedTarget target = bestTarget(pipeline);
+    AnalogGyro gyro = new AnalogGyro(0);
+    private DoubleLogEntry pitchLog = new DoubleLogEntry(DataLogManager.getLog(), "/log/input/pitch");
+    private DoubleLogEntry yawlog = new DoubleLogEntry(DataLogManager.getLog(), "/log/input/yaw");
+    private DoubleLogEntry distancelog = new DoubleLogEntry(DataLogManager.getLog(), "/log/input/distance");
+    private DoubleLogEntry poselog = new DoubleLogEntry(DataLogManager.getLog(), "/log/input/pose");
 
-//    private DoubleLogEntry pitchLog = new DoubleLogEntry(DataLogManager.getLog(),  "/log/input/pitch");
-//    private DoubleLogEntry yawlog = new DoubleLogEntry(DataLogManager.getLog(),  "/log/input/yaw");
-//    private DoubleLogEntry distancelog = new DoubleLogEntry(DataLogManager.getLog(),  "/log/input/distance");
-//    private DoubleLogEntry poselog = new DoubleLogEntry(DataLogManager.getLog(),  "/log/input/pose");
+    private PhotonVision() {
+        aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+        photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+                camera, robotToCam);
+    }
 
-   private PhotonVision(){
-    aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-    photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.LOWEST_AMBIGUITY, camera, robotToCam);
-    result = camera.getLatestResult();
-    target = result.getBestTarget();
-   }
-
-   public static PhotonVision getInstance(){
-        if (instance == null) {
-            instance = new PhotonVision();
+    public static PhotonVision getInstance() {
+        if (pvisioninstance == null) {
+            pvisioninstance = new PhotonVision();
         }
-        return instance;
+
+        return pvisioninstance;
     }
 
-    public void update(){
-        result = camera.getLatestResult();
-        target = result.getBestTarget();
+    public PhotonPipelineResult getPipeline() {
+        return camera.getLatestResult();
     }
 
-    public boolean foundTarget(){
-        boolean hasTargets = result.hasTargets();
-        //if(!hasTargets) System.out.println("ERROR: No Targets Found");
-        return hasTargets;
+    public boolean hasTarget(PhotonPipelineResult pipeline) {
+        return pipeline.hasTargets();
     }
 
-    public int getTagID(){
-        update();
-        if(!foundTarget()) return 0;
+    public double getYaw(PhotonTrackedTarget target) {
+        return target.getYaw();
+    }
 
-        return target.getFiducialId();
+    public double getPitch(PhotonTrackedTarget target) {
+        return target.getPitch();
+    }
+
+    public PhotonTrackedTarget bestTarget(PhotonPipelineResult result) {
+        return result.getBestTarget();
     }
 
     public double getDistance() {
-        update();
-        if(!foundTarget()) return 0;
 
+        PhotonTrackedTarget target = bestTarget(getPipeline());
         double distance = PhotonUtils.calculateDistanceToTargetMeters(cameraHeight, targetHeight, cameraPitch,
                 Units.degreesToRadians(target.getPitch()));
         return distance;
     }
 
-    public Pose3d getRobotPose3d(){
-        update();
-        if(!foundTarget()) return null;
-        
-        Optional<EstimatedRobotPose> pose = photonPoseEstimator.update();
-        if(pose.isPresent()){
-            return pose.get().estimatedPose;
+    public Pose3d getRobotPose3d() {
+        //TODO: If has no target, the numbers freeze, so make it so that savedResult gets cleared
+        Optional<EstimatedRobotPose> result = photonPoseEstimator.update();
+        if (result.isPresent()) {
+            savedResult = result.get().estimatedPose;
+            return savedResult;
+        } else {
+            return savedResult;
         }
-        else{
-            return new Pose3d(0,0,0, new Rotation3d(0.0, 0.0, 0.0));
-        }
-        
+
     }
 
-    public Pose2d getRobotPose2d(){
-        update();
-        if(!foundTarget()) return null;
+    public Transform3d getTransformToTarget(){
+        PhotonPipelineResult pipeline = getPipeline();
+        if(!pipeline.hasTargets()){
+            return new Transform3d(0.0, 0.0, 0.0, new Rotation3d());
+        }
+        PhotonTrackedTarget target = bestTarget(getPipeline());
+        SmartDashboard.putNumber("pose ambiguity", target.getPoseAmbiguity());
+        return target.getBestCameraToTarget();
+    }
 
-        // This tries to use PhotonUtils.estimateFieldToRobot() to get the robot's Pose2d.
+    public double get3dDist(){
+        return getTransformToTarget().getTranslation().getNorm();
+    }
+
+    public Pose2d getRobotPose2d() {
+        // This tries to use PhotonUtils.estimateFieldToRobot() to get the robot's
+        // Pose2d.
         // I followed the PhotonVision documentation:
         // https://docs.photonvision.org/en/latest/docs/programming/photonlib/getting-target-data.html
 
-        // TO-DO: Test this on robot and fix anything that needs fixing for this to work.
+        // TO-DO: Test this on robot and fix anything that needs fixing for this to
+        // work.
 
         // these shouldn't need tuning.
-        PhotonPipelineResult result = camera.getLatestResult();
-        PhotonTrackedTarget target = null;
-        if(result.hasTargets()) target = result.getBestTarget();
-        if(target == null) return null;
-
         double targetPitch = Units.degreesToRadians(target.getPitch());
         Rotation2d targetYaw = Rotation2d.fromDegrees(-target.getYaw());
 
         // need the pose of the AprilTag. somehow use TargetID to get the pose.
-        //int targetID = target.getFiducialId();
+        // int targetID = target.getFiducialId();
         Pose2d targetPose = new Pose2d(-2, 3, new Rotation2d());
 
         // need to tune this constant later
         Transform2d cameraToRobot = new Transform2d(0.5, 0.5, new Rotation2d());
 
         // PhotonVision's built in Pose2D estimator. Need to have actual parameters.
-        // the gyro.getRotation2d() has to be rechecked; not sure if this is the right way to implement.
+        // the gyro.getRotation2d() has to be rechecked; not sure if this is the right
+        // way to implement.
         Pose2d robotPose = PhotonUtils.estimateFieldToRobot(
-            cameraHeight, targetHeight, cameraPitch, targetPitch, targetYaw, gyro.getRotation2d(), targetPose, cameraToRobot
-        );
+                cameraHeight, targetHeight, cameraPitch, targetPitch, targetYaw, gyro.getRotation2d(), targetPose,
+                cameraToRobot);
 
         // for testing: try displaying the robotPose
         return robotPose;
     }
-    
-    public void periodic(){
-        //System.out.println("Testing the output in terminal");
-        try{
-            SmartDashboard.putNumber("Tag ID", getTagID());
-            // SmartDashboard.putNumber("PoseX", getRobotPose3d().getX());
-            // SmartDashboard.putNumber("PoseY", getRobotPose3d().getY());
-            // SmartDashboard.putNumber("PoseZ", getRobotPose3d().getZ());
-            // SmartDashboard.putNumber("Rot Z", getRobotPose3d().getRotation().getAngle());
-            
-        }catch(Exception e) {
 
-            // StringWriter sw = new StringWriter();
-            // PrintWriter pw = new PrintWriter(sw);
-            // e.printStackTrace(pw);
-            // String sStackTrace = sw.toString(); // stack trace as a string
-            // SmartDashboard.putString("Exception yar", sStackTrace);
-            // SmartDashboard.putNumber("PoseX", 0);
-            // SmartDashboard.putNumber("PoseY", 0);
-            // SmartDashboard.putNumber("PoseZ", 0);
-            // SmartDashboard.putNumber("Rot Z", 0);
-        }
+    public void periodic() {
+
+        SmartDashboard.putNumber("PoseX", getRobotPose3d().getX());
+        SmartDashboard.putNumber("PoseY", getRobotPose3d().getY());
+        SmartDashboard.putNumber("PoseZ", getRobotPose3d().getZ());
+        SmartDashboard.putNumber("Rot Z", getRobotPose3d().getRotation().getAngle());
+        // SmartDashboard.putNumber("Transform X", getTransformToTarget().getTranslation().getX());
+        // SmartDashboard.putNumber("Transform Y", getTransformToTarget().getTranslation().getY());
+        // SmartDashboard.putNumber("Transform Z", getTransformToTarget().getTranslation().getZ());
+        // SmartDashboard.putNumber("Transform Rot Angle", getTransformToTarget().getRotation().getAngle());
+        SmartDashboard.putNumber("Dist", get3dDist());
+
+        
+
+
     }
 
 }
