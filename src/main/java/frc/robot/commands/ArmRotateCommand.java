d9b5f93f9ed61782cca3920678674acf33335b03
raroopa <roopa.rajagopalan1@gmail.com>
2024-01-22 20:58:46 -0800

    adjoodawjoidaw

--- src/main/java/frc/robot/commands/ArmRotateCommand.java
+++ src/main/java/frc/robot/commands/ArmRotateCommand.java
@@ -5,11 +5,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.ArmSubsystem;
 
-public class ArmToDegree extends Command {
+public class ArmRotateCommand extends Command {
   private final ArmSubsystem armSubsystem;
   private double angle;
 
-  public ArmToDegree(ArmSubsystem armSubsystem, double angle) {
+  public ArmRotateCommand(ArmSubsystem armSubsystem, double angle) {
     this.angle = angle;
     this.armSubsystem = armSubsystem;
     addRequirements(armSubsystem);
