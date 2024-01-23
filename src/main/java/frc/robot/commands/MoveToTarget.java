b132f87d6ab769f8398fcc3667ae006dccf67190
pastamaple <pastamaple73@gmail.com>
2024-01-22 18:39:57 -0800

    yes

--- src/main/java/frc/robot/commands/MoveToTarget.java
+++ src/main/java/frc/robot/commands/MoveToTarget.java
@@ -1,16 +1,20 @@
 package frc.robot.commands;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -20,38 +24,15 @@ import frc.robot.subsystems.SwerveSubsystem;
 public class MoveToTarget extends Command {
     private Pose2d targetLocation;
     private SwerveSubsystem swerve;
+    private Command pathFindingCommand;
 
     public MoveToTarget(SwerveSubsystem swerve, Pose2d targetLocation) {
         this.swerve = SwerveSubsystem.getInstance();
         this.targetLocation = targetLocation;
-        // Adds the subsystem as a requirement (prevents two commands from acting on
-        // subsystem at once)
         addRequirements(swerve);
     }
-    // Create a list of bezier points from poses. Each pose represents one waypoint.
-    // // The rotation component of the pose should be the direction of travel. Do
-    // not use holonomic rotation.
-    // bezierPoints = PathPlannerPath.bezierFromPoses(
-    // new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
-    // new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
-    // new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90))
-    // );List<Translation2d>
 
-    // // Create the path using the bezier points created above
-    // PathPlannerPath path = new PathPlannerPath(
-    // bezierPoints,
-    // new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI), // The constraints
-    // for this path. If using a differential drivetrain, the angular constraints
-    // have no effect.
-    // new GoalEndState(0.0, Rotation2d.fromDegrees(-90)) // Goal end state. You can
-    // set a holonomic rotation here. If using a differential drivetrain, the
-    // rotation will have no effect.
-    // );
-
-    // // Prevent the path from being flipped if the coordinates are already correct
-    // path.preventFlipping =true;
-
-    private Command pathFindingCommand;
+    
 
     @Override
     public void initialize() {
@@ -87,4 +68,52 @@ public class MoveToTarget extends Command {
     public boolean isFinished() {
         return pathFindingCommand == null ? false : pathFindingCommand.isFinished();
     }
+
+    // SmartDashboard.putData("Example Auto", new PathPlannerAuto("Example Auto"));
+
+    // // Add a button to run pathfinding commands to SmartDashboard
+    // AutoBuilder.pathfindToPose(
+    //   new Pose2d(14.0, 6.5, Rotation2d.fromDegrees(0)), 
+    //   new PathConstraints(
+    //     4.0, 4.0, 
+    //     Units.degreesToRadians(360), Units.degreesToRadians(540)
+    //   ), 
+    //   0, 
+    //   2.0
+    // );
+    // SmartDashboard.putData("Pathfind to Scoring Pos", AutoBuilder.pathfindToPose(
+    //   new Pose2d(2.15, 3.0, Rotation2d.fromDegrees(180)), 
+    //   new PathConstraints(
+    //     4.0, 4.0, 
+    //     Units.degreesToRadians(360), Units.degreesToRadians(540)
+    //   ), 
+    //   0, 
+    //   0
+    // ));
+
+    // // Add a button to SmartDashboard that will create and follow an on-the-fly path
+    // // This example will simply move the robot 2m in the +X field direction
+    // SmartDashboard.putData("On-the-fly path", Commands.runOnce(() -> {
+    //   Pose2d currentPose = swerve.getPose();
+      
+    //   // The rotation component in these poses represents the direction of travel
+    //   Pose2d startPos = new Pose2d(currentPose.getTranslation(), new Rotation2d());
+    //   Pose2d endPos = new Pose2d(currentPose.getTranslation().plus(new Translation2d(2.0, 0.0)), new Rotation2d());
+
+    //   List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(startPos, endPos);
+    //   PathPlannerPath path = new PathPlannerPath(
+    //     bezierPoints, 
+    //     new PathConstraints(
+    //       4.0, 4.0, 
+    //       Units.degreesToRadians(360), Units.degreesToRadians(540)
+    //     ),  
+    //     new GoalEndState(0.0, currentPose.getRotation())
+    //   );
+
+    //   // Prevent this path from being flipped on the red alliance, since the given positions are already correct
+    //   path.preventFlipping = true;
+
+    //   AutoBuilder.followPath(path).schedule();
+    // }));
+//   }
 }
