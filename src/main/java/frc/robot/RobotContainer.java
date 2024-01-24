4e656afcf3a076a94571d830d1e9bd66c3746f57
Ritvik Setty <ritviksetty@gmail.com>
2024-01-23 20:59:06 -0800

    Phoenix6 swerve (#7)
    * nothing
    
    * rudimentery swerve phoenix 6 code
    
    * ps4 + offsets
    
    * updated encoder offsets and pid tuned steer
    
    * added slewratelimited for swerve code. Moved most of code to its own command/subsystem
    
    * working swerve!
    
    * cleaned up code and deleted unwanted comments
    
    * removed sim code and added speed change based on L2/R2 input.
    
    * base auto code for path planning done
    
    * changed current limiter
    
    * fixed anthony's suggestions
    
    * changed swerve constatns to be in its own subclass
    
    * format...
    
    * format all code
    
    * added comments for the future to change constants for the new robot
    
    * odometry
    
    * new path for testing path planner
    
    * fixed auton path
    
    * yes
    
    * straight auton path
    
    * yes
    
    * new straight
    
    * massive commit
    
    * yes
    
    * yes
    
    * swerve module optimziabutoiuafd
    
    * curved auton path
    
    * yes
    
    * yes
    
    * got rid of auton code
    
    * commit to save
    
    * auton commit to save
    
    * fixed odometry directions (previously flipped)
    
    * working auton
    
    * ran `./gradlew :spotlessApply` for formatting
    
    * deleted profiling .hprof file
    
    * fixed swerve pr changes. did not change auton pr changes.
    
    * changed all constants to all caps
    
    * formatting setting changes
    
    * PR Changes
    
    * changed constants and code bug
    
    ---------
    
    Co-authored-by: pastamaple <pastamaple73@gmail.com>
    Co-authored-by: vivaanm16 <vivphone650@gmail.com>
    Co-authored-by: ahoysal <ahoysal855@student.fuhsd.org>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,14 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.commands.SwerveJoystickCommand;
+import frc.robot.subsystems.SwerveSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,45 +20,54 @@ import frc.robot.subsystems.ExampleSubsystem;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  /* Setting up bindings for necessary control of the swerve drive platform */
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final CommandPS4Controller joystick =
+      new CommandPS4Controller(Constants.OI.JOYSTICK_PORT);
+  private final SwerveSubsystem driveTrain = SwerveSubsystem.getInstance();
+
+  public final Telemetry logger = new Telemetry();
+
+  // Starts telemetry operations (essentially logging -> look on SmartDashboard, AdvantageScope)
+  public void doTelemetry() {
+    logger.telemeterize(driveTrain.getState());
+  }
+
+  private void configureBindings() {
+    SwerveJoystickCommand swerveJoystickCommand =
+        new SwerveJoystickCommand(
+            () -> -joystick.getRawAxis(1),
+            () -> -joystick.getRawAxis(0),
+            () -> -joystick.getRawAxis(2),
+            () -> (joystick.getRawAxis(3) - joystick.getRawAxis(4) + 2d) / 2d + 0.5,
+            driveTrain);
+    driveTrain.setDefaultCommand(swerveJoystickCommand);
+
+    // zero-heading
+    joystick
+        .circle()
+        .onTrue(
+            driveTrain.runOnce(
+                () ->
+                    driveTrain.seedFieldRelative(
+                        new Pose2d(new Translation2d(0, 0), new Rotation2d(0)))));
+    driveTrain.registerTelemetry(logger::telemeterize);
+  }
+
   public RobotContainer() {
-    // Configure the trigger bindings
+
+    // Vibrate joysticks when someone interesting happens!
+    // joystick.getHID().setRumble(GenericHID.RumbleType.kRightRumble, 1);
+    // joystick.getHID().setRumble(GenericHID.RumbleType.kLeftRumble, 1);
+
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    // autonomous command applies brake
+    // final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    // return driveTrain.applyRequest(() -> brake);
+    return new PathPlannerAuto("New Auto");
   }
 }
