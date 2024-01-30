0e44c1edb005cf7d75dd5b8686b25c7f6aa24b74
pastamaple <pastamaple73@gmail.com>
2024-01-29 17:40:27 -0800

    pose reflection fix

--- src/main/java/frc/robot/util/MiscUtils.java
+++ src/main/java/frc/robot/util/MiscUtils.java
@@ -11,9 +11,8 @@ public class MiscUtils {
    */
   public static Pose2d reflectAcrossMidline(Pose2d point) {
     return new Pose2d(
-        point.getX() + (2.0 * (point.getX() - Constants.Landmarks.CENTER_LINE_LOCATION)),
+        point.getX() - (2.0 * (point.getX() - Constants.Landmarks.CENTER_LINE_LOCATION)),
         point.getY(),
-        new Rotation2d(
-            point.getRotation().getDegrees() + (2.0 * (90.0 - point.getRotation().getDegrees()))));
+        new Rotation2d(Math.PI - point.getRotation().getRadians()));
   }
 }
