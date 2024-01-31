193f91a5a0a1e3d831b567976a6fd839666a33b2
anthonyl232 <113406648+anthonyl232@users.noreply.github.com>
2024-01-30 19:59:17 -0800

    Phoenix6 swerve autonandtarget (#19)
    * nothing
    
    * rudimentery swerve phoenix 6 code
    
    * ps4 + offsets
    
    * updated encoder offsets and pid tuned steer
    
    * added slewratelimited for swerve code. Moved most of code to its own command/subsystem
    
    * working swerve!
    
    * cleaned up code and deleted unwanted comments
    
    * removed sim code and added speed change based on L2/R2 input.
    
    * base auto code for path planning done
    
    * changed current limiter
    
    * fixed anthony's suggestions
    
    * changed swerve constatns to be in its own subclass
    
    * format...
    
    * format all code
    
    * added comments for the future to change constants for the new robot
    
    * odometry
    
    * new path for testing path planner
    
    * fixed auton path
    
    * yes
    
    * straight auton path
    
    * yes
    
    * new straight
    
    * massive commit
    
    * yes
    
    * yes
    
    * swerve module optimziabutoiuafd
    
    * curved auton path
    
    * yes
    
    * yes
    
    * got rid of auton code
    
    * commit to save
    
    * auton commit to save
    
    * fixed odometry directions (previously flipped)
    
    * working auton
    
    * yes
    
    * good
    
    * yes
    
    * yes
    
    * final
    
    * good
    
    * good
    
    * formatt
    
    * pull request changes
    
    * naming for auto paths
    
    * yes
    
    * made ppconstants subclass of swerve in constants
    
    * basic changes (before the robotcontainer changes)
    
    * robot container + new red auto change
    
    * pose reflection fix
    
    * ok
    
    * ok
    
    * constant namedchange
    
    ---------
    
    Co-authored-by: Rawpower9 <ritviksetty@gmail.com>
    Co-authored-by: pastamaple <pastamaple73@gmail.com>
    Co-authored-by: vivaanm16 <vivphone650@gmail.com>
    Co-authored-by: ahoysal <ahoysal855@student.fuhsd.org>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,10 +8,17 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.commands.MoveToTarget;
 import frc.robot.commands.SwerveJoystickCommand;
 import frc.robot.subsystems.SwerveSubsystem;
+import java.util.Optional;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,6 +27,85 @@ import frc.robot.subsystems.SwerveSubsystem;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+  /* Setting up bindings for necessary control of the swerve drive platform */
+
+  // Constructs a Pose2d array of the note locations by a specific indexing so they can be accessed
+  // by the eventual autonomous chooser
+  private enum NoteLocation {
+    AMPSIDE(Constants.Landmarks.AMPSIDE_NOTE_LOCATION),
+    MIDDLE(Constants.Landmarks.MIDDLE_NOTE_LOCATION),
+    STAGESIDE(Constants.Landmarks.STAGESIDE_NOTE_LOCATION);
+
+    private final Pose2d pose;
+
+    private NoteLocation(Pose2d pose) {
+      this.pose = pose;
+    }
+
+    private Pose2d getNoteLocation() {
+      return this.pose;
+    }
+  }
+
+  private static boolean redAlliance;
+
+  public static void setAlliance() {
+    redAlliance =
+        (DriverStation.getAlliance().isEmpty())
+            ? false
+            : (DriverStation.getAlliance().get() == Alliance.Red);
+  }
+
+  // Options on SmartDashboard that return an integer index that refers to a note location
+  private static SendableChooser<Optional<NoteLocation>>
+      pickup1choice = new SendableChooser<Optional<NoteLocation>>(),
+      pickup2choice = new SendableChooser<Optional<NoteLocation>>();
+
+  private void setupChooser() {
+    // // Instantiations
+    // pickup1choice = new SendableChooser<Integer>();
+    // pickup2choice = new SendableChooser<Integer>();
+
+    pickup1choice.setDefaultOption("SECOND SHOT: DO NOTHING", Optional.empty());
+    pickup1choice.addOption("AMPSIDE", Optional.of(NoteLocation.AMPSIDE));
+    pickup1choice.addOption("MIDDLE", Optional.of(NoteLocation.MIDDLE));
+    pickup1choice.addOption("STAGESIDE NOTE", Optional.of(NoteLocation.STAGESIDE));
+
+    pickup2choice.setDefaultOption("THIRD SHOT: DO NOTHING", Optional.empty());
+    pickup2choice.addOption("AMPSIDE NOTE", Optional.of(NoteLocation.AMPSIDE));
+    pickup2choice.addOption("MIDDLE NOTE", Optional.of(NoteLocation.MIDDLE));
+    pickup2choice.addOption("STAGESIDE NOTE", Optional.of(NoteLocation.STAGESIDE));
+
+    SmartDashboard.putData(pickup1choice);
+    SmartDashboard.putData(pickup2choice);
+  }
+
+  public RobotContainer() {
+    // Vibrate joysticks when someone interesting happens!
+    // joystick.getHID().setRumble(GenericHID.RumbleType.kRightRumble, 1);
+    // joystick.getHID().setRumble(GenericHID.RumbleType.kLeftRumble, 1);
+
+    configureBindings();
+    setupChooser();
+  }
+
+  public Command getAutonomousCommand() {
+
+    String autonName = (redAlliance) ? "ThreeNoteAutonRed" : "ThreeNoteAutonBlue";
+    SmartDashboard.putString("Auton to be run", autonName);
+    SmartDashboard.putBoolean("Red Alliance?", redAlliance);
+    return new PathPlannerAuto(autonName)
+        .andThen(
+            (pickup1choice.getSelected().isEmpty())
+                ? new WaitCommand(2.0)
+                : MoveToTarget.withMirror(
+                    driveTrain, pickup1choice.getSelected().get().getNoteLocation(), redAlliance))
+        .andThen(
+            (pickup2choice.getSelected().isEmpty())
+                ? new WaitCommand(2.0)
+                : MoveToTarget.withMirror(
+                    driveTrain, pickup2choice.getSelected().get().getNoteLocation(), redAlliance));
+  }
 
   /* Setting up bindings for necessary control of the swerve drive platform */
 
@@ -37,8 +123,8 @@ public class RobotContainer {
   private void configureBindings() {
     SwerveJoystickCommand swerveJoystickCommand =
         new SwerveJoystickCommand(
-            () -> -joystick.getRawAxis(1),
-            () -> -joystick.getRawAxis(0),
+            () -> ((redAlliance) ? joystick.getRawAxis(1) : -joystick.getRawAxis(1)),
+            () -> ((redAlliance) ? joystick.getRawAxis(0) : -joystick.getRawAxis(0)),
             () -> -joystick.getRawAxis(2),
             () -> (joystick.getRawAxis(3) - joystick.getRawAxis(4)) / 4d + 0.5,
             driveTrain);
@@ -53,21 +139,5 @@ public class RobotContainer {
                     driveTrain.seedFieldRelative(
                         new Pose2d(new Translation2d(0, 0), new Rotation2d(0)))));
     driveTrain.registerTelemetry(logger::telemeterize);
-  }
-
-  public RobotContainer() {
-
-    // Vibrate joysticks when someone interesting happens!
-    // joystick.getHID().setRumble(GenericHID.RumbleType.kRightRumble, 1);
-    // joystick.getHID().setRumble(GenericHID.RumbleType.kLeftRumble, 1);
-
-    configureBindings();
-  }
-
-  public Command getAutonomousCommand() {
-    // autonomous command applies brake
-    // final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    // return driveTrain.applyRequest(() -> brake);
-    return new PathPlannerAuto("New Auto");
   }
 }
