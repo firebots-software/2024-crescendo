63648a11db12fa900c3563d6745d6478e168e28d
mahikamaini <93836668+DuchessBanana@users.noreply.github.com>
2024-02-15 21:16:34 -0800

    Co-authored-by: raroopa <raroopa@users.noreply.github.com> Co-authored-by: Dan36252 <Dan36252@users.noreply.github.com>
    tuned feedforward, acc, p-value; added all 4 motors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,14 +69,23 @@ public final class Constants {
     public static final double DEFAULT_ARM_ANGLE = 15;
     public final double INTAKE_ANGLE = 0; // subject to change
     public final double AMP_ANGLE = 100; // subject to change
+    public static final double SPEAKER_ANGLE = 40; // TODO: Replace with the function based on distance
     //public static final double ARM_ENCODER_OFFSET = 0; // TODO: Change the offset so that the 0 position is when the arm is at its resting
     // position.
-    public static final double SPEAKER_ANGLE = 40; // TODO: Replace with the function based on distance
+    
     public static final int RT_PORT = 14; // Right Top motor
     public static final int RB_PORT = 13; // Right Bottom motor
     public static final int LT_PORT = 12; // Left Top motor
     public static final int LB_PORT = 11; // Left Bottom motor
     public static final int ENCODER_PORT = 0; // subject to change
+
+    public static final double CURRENT_LIMIT = 5.0;
+    public static final double S0C_KP = 18.5;
+    public static final double ARMFF_KS = 0.1;
+    public static final double ARMFF_KG = 0.55;
+    public static final double ARMFF_KV = 1.45;
+    public static final double MOTIONMAGIC_KV = 1; // MotionMagic Cruise Velocity in RPS of the arm
+    public static final double MOTIONMAGIC_KA = 0.5; // MotionMagic Acceleration in RPS^2 of the arm
     
     public static final double ABSOLUTE_ARM_CONVERSION_FACTOR = 42d/18d;
     public static final double INTEGRATED_ABSOLUTE_CONVERSION_FACTOR = 34 + 2.0/3.0;
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,4 +1,6 @@
 package frc.robot.subsystems;
+import javax.swing.text.StyleContext.SmallAttributeSet;
+
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -15,52 +17,61 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ArmSubsystem extends SubsystemBase {
-  private TalonFX r1, r2, l1, l2;
-  private TalonFX master;
-  private DutyCycleEncoder revEncoder;
-  private ArmFeedforward armff;
-  private MotionMagicConfigs mmc;
   private static ArmSubsystem instance;
 
-  private boolean initialize = false;
+  private TalonFX rt, rb, lt, lb;
+  private TalonFX master;
+  private DutyCycleEncoder revEncoder;
+
+  private ArmFeedforward armff;
+  private MotionMagicConfigs mmc;
+
+  private boolean initialized = false;
   
   private double targetDegrees;
-  private double absEncPretzelClamp = Constants.Arm.ABSOLUTE_ENCODER_HORIZONTAL*8/10d;
+  //private double absEncPretzelClamp = Constants.Arm.ABSOLUTE_ENCODER_HORIZONTAL*8/10d;
   private double armHorizontalOffset;
 
   public ArmSubsystem() {
-    CurrentLimitsConfigs clc = new CurrentLimitsConfigs().withSupplyCurrentLimit(5.0);
-
-    Slot0Configs s0c = new Slot0Configs().withKP(37).withKI(0).withKD(0);
-    armff = new ArmFeedforward(0.1, 0.1, 0.1);
-    r1 = new TalonFX(Constants.Arm.RT_PORT, Constants.Swerve.CANBUS_NAME);
-    // r2 = new TalonFX(Constants.Arm.RB_PORT, Constants.Swerve.CANBUS_NAME);
-    l1 = new TalonFX(Constants.Arm.LT_PORT, Constants.Swerve.CANBUS_NAME);
-    // l2 = new TalonFX(Constants.Arm.LB_PORT, Constants.Swerve.CANBUS_NAME);
-
-     Follower follower = new Follower(Constants.Arm.LT_PORT, true);
-     r1.setControl(follower);
-    // r2.setControl(f);
-    // l2.setControl(follower);
-
+    // Initialize Current Limit, Slot0Configs, and ArmFeedForward
+    CurrentLimitsConfigs clc = new CurrentLimitsConfigs().withSupplyCurrentLimit(Constants.Arm.CURRENT_LIMIT);
+    Slot0Configs s0c = new Slot0Configs().withKP(Constants.Arm.S0C_KP).withKI(0).withKD(0);
+    armff = new ArmFeedforward(Constants.Arm.ARMFF_KS, Constants.Arm.ARMFF_KG, Constants.Arm.ARMFF_KV);
     
+    // Initialize motors
+    rt = new TalonFX(Constants.Arm.RT_PORT, Constants.Swerve.CANBUS_NAME);
+    rb = new TalonFX(Constants.Arm.RB_PORT, Constants.Swerve.CANBUS_NAME);
+    lt = new TalonFX(Constants.Arm.LT_PORT, Constants.Swerve.CANBUS_NAME);
+    lb = new TalonFX(Constants.Arm.LB_PORT, Constants.Swerve.CANBUS_NAME);
 
-    r1.getConfigurator().apply(clc);
-    // r2.getConfigurator().apply(clc);
-    l1.getConfigurator().apply(clc);
-    // l2.getConfigurator().apply(clc);
+    // Set up motor followers and deal with inverted motors
+    Follower follower = new Follower(Constants.Arm.LT_PORT, true);
+    Follower invertedFollower = new Follower(Constants.Arm.LT_PORT, false);
+    rt.setControl(follower);
+    rb.setControl(follower);
+    lb.setControl(invertedFollower);
 
-    master = l1;
+    // Apply Current Limit to all motors
+    rt.getConfigurator().apply(clc);
+    rb.getConfigurator().apply(clc);
+    lt.getConfigurator().apply(clc);
+    lb.getConfigurator().apply(clc);
+
+    // Assign master motor and apply Slot0Configs to master
+    master = lt;
     TalonFXConfigurator masterConfigurator = master.getConfigurator();
     masterConfigurator.apply(s0c);
 
+    // Apply MotionMagicConfigs to master motor
     mmc = new MotionMagicConfigs();
-    mmc.MotionMagicCruiseVelocity = 1*Constants.Arm.INTEGRATED_ARM_CONVERSION_FACTOR;
-    mmc.MotionMagicAcceleration = 0.25*Constants.Arm.INTEGRATED_ARM_CONVERSION_FACTOR;
-    mmc.MotionMagicJerk = 1600;
+    mmc.MotionMagicCruiseVelocity = Constants.Arm.MOTIONMAGIC_KV*Constants.Arm.INTEGRATED_ARM_CONVERSION_FACTOR;
+    mmc.MotionMagicAcceleration = Constants.Arm.MOTIONMAGIC_KA*Constants.Arm.INTEGRATED_ARM_CONVERSION_FACTOR;
+    //mmc.MotionMagicJerk = 1600;
     masterConfigurator.apply(mmc);
 
+    // Initialize absolute encoder
     revEncoder = new DutyCycleEncoder(Constants.Arm.ENCODER_PORT);
+
     // ==== EXPLANATION: ====
     // getAbsolutePosition(): Absolute Encoder's current reading
     // ABSOLUTE_ENCODER_HORIZONTAL: What the Absolute Encoder reads at horizontal
@@ -70,13 +81,18 @@ public class ArmSubsystem extends SubsystemBase {
     // it represents the arm being at horizontal. After this next line runs, the Master motor's encoder reading can be used
     // like expected, so you simply need to divide its reading by INTEGRATED_ARM_CONVERSION_FACTOR to get the arm's angle in rotations.
     // ======================
+
+    // In a new thread, wait until absolute encoder is connected.
+    // Then, set Master's position to the absolute encoder's position, converted to motor rotations.
+    // Finally, initialize the armHorizontalOffset variable, which is the positive offset the arm reads
+    // as its angle when it's horizontal, in rotations.
     new Thread(() -> {
       try {
         do {
           Thread.sleep(250);
         } while (!revEncoder.isConnected());
         master.setPosition((getAbsolutePosition())*Constants.Arm.INTEGRATED_ABSOLUTE_CONVERSION_FACTOR);
-        initialize = true;
+        initialized = true;
         armHorizontalOffset = Constants.Arm.ABSOLUTE_HORIZONTAL_OFFSET/Constants.Arm.ABSOLUTE_ARM_CONVERSION_FACTOR;
         
       } catch (InterruptedException e) {
@@ -85,6 +101,9 @@ public class ArmSubsystem extends SubsystemBase {
     }
     ).run();
     
+    // POTENTIAL PROBLEM:
+    /* The way we take the Absolute Encoder's offsets.
+    */
 
     targetDegrees = Constants.Arm.DEFAULT_ARM_ANGLE;
   }
@@ -97,17 +116,27 @@ public class ArmSubsystem extends SubsystemBase {
   }
 
   private void setPosition(double angleDegrees) {
-    if(initialize)
+    if(initialized)
     {
-      master.setControl(new MotionMagicVoltage(getIntegratedTargetRots(angleDegrees)));
+      master.setControl(new MotionMagicVoltage(getIntegratedTargetRots(angleDegrees))
+      .withFeedForward(armff.calculate((2*Math.PI*getArmDegrees())/360d, 0)));
     }
-    // .withFeedForward(armff.calculate(getPosRotations() * Math.PI * 2 / 360, 0)));
-    // input is in rotations
   }
 
-  private double getIntegratedTargetRots(double angleDegrees){
-    double armRots = angleDegrees/360d + armHorizontalOffset;
-    return armRots * Constants.Arm.INTEGRATED_ARM_CONVERSION_FACTOR;
+  public void setTargetDegrees(double angleDegrees) {
+    targetDegrees = angleDegrees;
+  }
+
+  // public double determineAngle(Pose2d a, double fkla) {
+  //   return -1;
+  // }
+
+  public void rotateArmToSpeakerPosition() {
+    setTargetDegrees(Constants.Arm.SPEAKER_ANGLE);
+  }
+
+  public void rotateArmToRestPosition() {
+    setTargetDegrees(Constants.Arm.DEFAULT_ARM_ANGLE);
   }
 
   private double getAbsolutePosition() {
@@ -115,7 +144,7 @@ public class ArmSubsystem extends SubsystemBase {
   }
 
   public double getPosRotations() {
-    if(!initialize){
+    if(!initialized){
       System.out.println("WARNING: Motor Position looked at, but initialization not complete yet. Returning 0");
       return 0;
     }
@@ -127,58 +156,31 @@ public class ArmSubsystem extends SubsystemBase {
     return getPosRotations() / Constants.Arm.INTEGRATED_ARM_CONVERSION_FACTOR;
   }
 
-  public void setTargetDegrees(double angleDegrees) {
-    targetDegrees = angleDegrees;
-  }
-
-  public double determineAngle(Pose2d a, double fkla) {
-    return -1;
-  }
-
-  public void rotateArmToSpeakerPosition() {
-    setTargetDegrees(Constants.Arm.SPEAKER_ANGLE);
-  }
-
-  public void rotateArmToRestPosition() {
-    setTargetDegrees(Constants.Arm.DEFAULT_ARM_ANGLE);
-  }
-
   public double getArmDegrees()
   {
     return getArmPosRotations() * 360d;
   }
 
-  // public void toPosition() {
-  //   // Magic Motion:
-  //   MotionMagicVoltage m_request = new MotionMagicVoltage(master.getPosition().getValue());
-  //   master.setControl(m_request.withPosition(setPos));
-
-  // Trapizoidal Motion:
-  // TrapezoidProfile.State setPoint = new TrapezoidProfile.State(setPos, 0);
-  // TrapezoidProfile.State currentPoint = new
-  // TrapezoidProfile.State(master.getPosition().getValue(),master.getVelocity().getValue());
-
-  // setPoint = profile.calculate(profile.totalTime(), currentPoint, setPoint);
-  // PositionDutyCycle m_positionControl = new
-  // PositionDutyCycle(setPoint.position);
-  // m_positionControl.Position = setPoint.position;
-  // m_positionControl.Velocity = setPoint.velocity;
-  // master.setControl(m_positionControl);
-  // }
+  private double getIntegratedTargetRots(double angleDegrees){
+    double armRots = angleDegrees/360d + armHorizontalOffset;
+    return armRots * Constants.Arm.INTEGRATED_ARM_CONVERSION_FACTOR;
+  }
 
   @Override
   public void periodic() {
     setPosition(targetDegrees);;
-    SmartDashboard.putString("Command:", this.getCurrentCommand() == null ? "none" : this.getCurrentCommand().getName());
+    SmartDashboard.putString("ARM Command:", this.getCurrentCommand() == null ? "none" : this.getCurrentCommand().getName());
     
-    SmartDashboard.putNumber("Absolute Raw", revEncoder.getAbsolutePosition());
-    SmartDashboard.putNumber("Absolute via Function", getAbsolutePosition());
-    SmartDashboard.putNumber("Integrated Rotations: ", getPosRotations());
-    SmartDashboard.putNumber("Integrated Current: ", master.getSupplyCurrent().getValue());
-    SmartDashboard.putNumber("Integrated Error: ", master.getClosedLoopError().getValue());
-    SmartDashboard.putNumber("Target Degrees: ", targetDegrees);
-    SmartDashboard.putNumber("Arm Rotations: ", getArmPosRotations());
-    SmartDashboard.putNumber("Arm Degrees", getArmDegrees());
-    SmartDashboard.putNumber("Target Integrated Rots: ", getIntegratedTargetRots(targetDegrees));
+    SmartDashboard.putNumber("ARM Abs Enc Raw: ", revEncoder.getAbsolutePosition());
+    SmartDashboard.putNumber("ARM Abs Enc Func: ", getAbsolutePosition());
+    SmartDashboard.putNumber("ARM Integrated Rotations: ", getPosRotations());
+    SmartDashboard.putNumber("ARM Integrated Current: ", master.getSupplyCurrent().getValue());
+    SmartDashboard.putNumber("ARM Integrated Error: ", master.getClosedLoopError().getValue());
+    SmartDashboard.putNumber("ARM Arm Rotations: ", getArmPosRotations());
+    SmartDashboard.putNumber("ARM Arm Degrees: ", getArmDegrees());
+    SmartDashboard.putNumber("ARM Arm Degrees Corrected: ", getArmDegrees()-(armHorizontalOffset*360d));
+    SmartDashboard.putNumber("ARM Target Degrees: ", targetDegrees);
+    SmartDashboard.putNumber("ARM Target Integrated Rots: ", getIntegratedTargetRots(targetDegrees));
+    SmartDashboard.putNumber("ARM FeedForward Calculations: ", armff.calculate((2*Math.PI*getArmDegrees())/360d, 0));
   }
 }
