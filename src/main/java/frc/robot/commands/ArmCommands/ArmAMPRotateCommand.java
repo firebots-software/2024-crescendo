6a9e258be8eab704243a373199d13ebf20133e82
krishnakem <krishna@kemisetti.com>
2024-02-20 16:14:24 -0800

    yes ritvik

--- src/main/java/frc/robot/commands/ArmCommands/ArmAMPRotateCommand.java
+++ src/main/java/frc/robot/commands/ArmCommands/ArmAMPRotateCommand.java
@@ -2,11 +2,9 @@ package frc.robot.commands.ArmCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.PeterSubsystem;
 
 public class ArmAMPRotateCommand extends Command {
   private ArmSubsystem armSubsystem;
-  private PeterSubsystem peterSubsystem;
 
   public ArmAMPRotateCommand(ArmSubsystem armSubsystem) {
     this.armSubsystem = armSubsystem;
@@ -25,13 +23,11 @@ public class ArmAMPRotateCommand extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    armSubsystem.rotateToRestPosition();
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return armSubsystem.atTarget(1);
   }
 }
