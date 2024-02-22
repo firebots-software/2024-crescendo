bf6b1770f91213d9d3425078b0e5beb8d09a0017
Venkata Siva Ramisetty <92191466+Ace-Kat@users.noreply.github.com>
2024-02-21 21:08:49 -0800

    Cleaned up vision code and added it to main. (#27)
    * Cleaned up vision code and added it to main.
    
    * Uncommented spotless for the pr
    
    * Added kalman filter
    
    * updated vision matrix values
    
    * We changed the camera to robot constants and removed vision matrix. need to fix it. VISION WORKS!!!!!!!!!!!!!!!!!1
    
    * Added robottocam pitch, yaw, and roll
    
    * Converted units in PhotonVision and cleaned up formatting and weird github merge decisions.
    
    * Current limits (#29)
    
    * constants changes
    
    * Stator current limits added
    
    * yes
    
    * yes
    
    * stator unlimit
    
    * default arm angle
    
    * default arm angle 20
    
    * current limits
    
    ---------
    
    Co-authored-by: ahoysal <ahoysal855@student.fuhsd.org>
    
    * with matrix
    
    * Branchforkalash (#30)
    
    * set Amp angle constant
    created new calculateAngleToSpeaker method
    Co-authored-by: krishnakem <krishnakem@users.noreply.github.com>
    
    * Created a command to angle arm to amp
    refactored some naming of commands in armSubsystem
    
    * unites
    
    * working mostly interpolation
    
    * Current limits (#29)
    
    * constants changes
    
    * Stator current limits added
    
    * yes
    
    * yes
    
    * stator unlimit
    
    * default arm angle
    
    * default arm angle 20
    
    * current limits
    
    ---------
    
    Co-authored-by: ahoysal <ahoysal855@student.fuhsd.org>
    
    * set Amp angle constant
    created new calculateAngleToSpeaker method
    Co-authored-by: krishnakem <krishnakem@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kalashvenku <Kalashvenku@gmail.com>
    Co-authored-by: pastamaple73 <103950944+pastamaple73@users.noreply.github.com>
    Co-authored-by: ahoysal <ahoysal855@student.fuhsd.org>
    
    * Cleaned up vision code and added it to main.
    
    ---------
    
    Co-authored-by: Warzone <yajwin.grover@gmail.com>
    Co-authored-by: pastamaple73 <103950944+pastamaple73@users.noreply.github.com>
    Co-authored-by: ahoysal <ahoysal855@student.fuhsd.org>
    Co-authored-by: krishnakem <krishna@kemisetti.com>
    Co-authored-by: Kalashvenku <Kalashvenku@gmail.com>
    Co-authored-by: Aagrim Hoysal <91349517+ahoysal@users.noreply.github.com>

--- src/main/java/frc/robot/Telemetry.java
+++ src/main/java/frc/robot/Telemetry.java
@@ -105,7 +105,7 @@ public class Telemetry {
     /* Telemeterize the pose */
     Pose2d pose = state.Pose;
     fieldTypePub.set("Field2d");
-    fieldPub.set(new double[] {pose.getX(), pose.getY(), pose.getRotation().getRotations()});
+    fieldPub.set(new double[] {pose.getX(), pose.getY(), pose.getRotation().getRadians()});
 
     /* Telemeterize the robot's general speeds */
     double currentTime = Utils.getCurrentTimeSeconds();
