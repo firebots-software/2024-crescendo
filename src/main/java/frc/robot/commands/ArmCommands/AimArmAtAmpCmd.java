f3b163cb05c7403cdff8dd2db78b586dbe123035
krishnakem <krishna@kemisetti.com>
2024-02-21 20:27:02 -0800

    Branchforkalash (#30)
    * set Amp angle constant
    created new calculateAngleToSpeaker method
    Co-authored-by: krishnakem <krishnakem@users.noreply.github.com>
    
    * Created a command to angle arm to amp
    refactored some naming of commands in armSubsystem
    
    * unites
    
    * working mostly interpolation
    
    * Current limits (#29)
    
    * constants changes
    
    * Stator current limits added
    
    * yes
    
    * yes
    
    * stator unlimit
    
    * default arm angle
    
    * default arm angle 20
    
    * current limits
    
    ---------
    
    Co-authored-by: ahoysal <ahoysal855@student.fuhsd.org>
    
    * set Amp angle constant
    created new calculateAngleToSpeaker method
    Co-authored-by: krishnakem <krishnakem@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kalashvenku <Kalashvenku@gmail.com>
    Co-authored-by: pastamaple73 <103950944+pastamaple73@users.noreply.github.com>
    Co-authored-by: ahoysal <ahoysal855@student.fuhsd.org>

--- src/main/java/frc/robot/commands/ArmCommands/AimArmAtAmpCmd.java
+++ src/main/java/frc/robot/commands/ArmCommands/AimArmAtAmpCmd.java
@@ -4,13 +4,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
-public class AimArmCmd extends Command {
+public class AimArmAtAmpCmd extends Command {
   private ArmSubsystem armSubsystem;
-  private SwerveSubsystem swerveSubsystem;
 
-  public AimArmCmd(ArmSubsystem armSubsystem, SwerveSubsystem swerveSubsystem) {
+  public AimArmAtAmpCmd(ArmSubsystem armSubsystem, SwerveSubsystem swerveSubsystem) {
     this.armSubsystem = armSubsystem;
-    this.swerveSubsystem = swerveSubsystem; // only used to get pose
     addRequirements(armSubsystem);
   }
 
@@ -21,7 +19,7 @@ public class AimArmCmd extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    armSubsystem.rotateToSpeaker(swerveSubsystem.getState().Pose.getTranslation());
+    armSubsystem.rotateToAmpPosition();
   }
 
   // Called once the command ends or is interrupted.
