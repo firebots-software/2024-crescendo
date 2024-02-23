6ba88dea440dda1ceae695b0bb25c7638caecf1d
pastamaple73 <anthonyyl.3am@gmail.com>
2024-02-23 11:07:17 -0800

    yes

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -128,8 +128,6 @@ public class SwerveSubsystem extends SwerveDrivetrain implements Subsystem {
   @Override
   public void periodic() {
     // Chassis Speeds information
-    SmartDashboard.putNumber("ChassisSpeedsX", getCurrentRobotChassisSpeeds().vxMetersPerSecond);
-    SmartDashboard.putNumber("ChassisSpeedsY", getCurrentRobotChassisSpeeds().vyMetersPerSecond);
     SmartDashboard.putNumber(
         "ChassisSpeedsRadians", getCurrentRobotChassisSpeeds().omegaRadiansPerSecond);
   }
