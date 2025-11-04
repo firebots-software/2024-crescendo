f706f107c22346c6aa2da03f163a57512e421309
73anthonyL <73anthony.lu@gmail.com>
2025-11-03 17:23:36 -0800

    stator lowered to 20

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -170,7 +170,7 @@ public final class Constants {
 
   public static final class Arm {
     public static final double BUNDT_ANGLE = 20d;
-    public static final double ARM_STATOR_CURRENT_LIMIT_AMPS = 40.0;
+    public static final double ARM_STATOR_CURRENT_LIMIT_AMPS = 20.0;
     public static final double DEFAULT_ARM_ANGLE = 56.12;
     public static final double INTAKE_ANGLE = 3; // subject to change
     public static final double AMP_ANGLE = 95; // subject to change
