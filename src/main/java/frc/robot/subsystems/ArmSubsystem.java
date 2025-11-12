2df3cedcf8abfb0b20c8244668e37374895fe06a
73anthonyL <73anthony.lu@gmail.com>
2025-11-12 15:41:44 -0800

    added good logging to the base

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -280,7 +280,8 @@ public class ArmSubsystem extends SubsystemBase {
 
 
     // TODO: ACTUAL LOGGING STATEMENTS FOR PID+MP ACTIVITY:
-    DogLog.log("MD.6-lab/X Variable", 3.0);
+    DogLog.log("MD.6-lab/Current_Arm_Angle_Degrees", getCorrectedDegrees());
+    SmartDashboard.putNumber("MD.6-lab/Target_Arm_Angle_Degrees", targetDegrees);
     periodicSignalLogger();
   }
 
